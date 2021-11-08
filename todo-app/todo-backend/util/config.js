116ddc422c104b58e3fa72fa9c372786eb9ad7f9
Izhan <babuizhan1234@gmail.com>
2021-11-08 16:02:54 +0530

    completed 7

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -2,6 +2,6 @@ const MONGO_URL = process.env.MONGO_URL || undefined;
 const REDIS_URL = process.env.REDIS_URL || undefined;
 
 module.exports = {
-  MONGO_URL: "mongodb://izhan:izhan@localhost:3456/todos",
+  MONGO_URL: "mongodb://localhost:3456/todos",
   REDIS_URL: "//localhost:6378",
 };
